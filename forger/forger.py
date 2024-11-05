d4c686613bf61f70f4a68e11827e3742745614e7
Marc Doerflinger <marc@doerf.li>
2024-11-05 13:57:37 +0000

    cleanup

--- forger/forger.py
+++ forger/forger.py
@@ -4,7 +4,7 @@ def main():
     mnemo = Mnemonic("english")
     random_seed = mnemo.generate(strength=128)
     print(random_seed)
-    print(mnemo.to_seed(random_seed))
+
 
 
 if __name__ == '__main__':
